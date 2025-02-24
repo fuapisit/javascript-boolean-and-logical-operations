da3a0fc85ea7e43844a18d414abc6e7f1cf517fc
Apisit <fu.apisit@gmail.com>
2025-02-24 15:12:30 +0700

    feat add answer ex1-3

--- ex-1.js
+++ ex-1.js
@@ -4,5 +4,12 @@ let hasCriminalBlacklist;
 let isAllow;
 
 // Start coding here
+let jamesAge = 18; 
+let hasCriminalRecord = false; 
+
+isOver18 = jamesAge > 18; 
+hasCriminalBlacklist = hasCriminalRecord; 
+
+isAllow = isOver18 && !hasCriminalBlacklist;
 
 console.log(isAllow);
--- ex-2.js
+++ ex-2.js
@@ -9,4 +9,23 @@ let isPlatinum;
 
 let hasPromotion;
 
+// กำหนดคุณสมบัติของ John
+let lastMonthSpending = 4001; 
+let purchaseDay = "Friday"; 
+let boughtITProduct = false; 
+let attendedDiscountEvent = true; 
+let membership = "Gold"; 
+
+// Assign ค่าให้กับตัวแปร Boolean ตามคุณสมบัติของ John
+lastMonthPaidMoreThan4000 = lastMonthSpending > 4000;
+isWeekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"].includes(purchaseDay);
+hasBoughtProductFromITCategory = boughtITProduct;
+hasAttendedDiscountEvent = attendedDiscountEvent;
+isPlatinum = membership === "Platinum";
+
+// ตรวจสอบสิทธิ์โปรโมชั่น
+hasPromotion = 
+  (lastMonthPaidMoreThan4000 && isWeekday && !hasBoughtProductFromITCategory && !hasAttendedDiscountEvent) || 
+  isPlatinum;
+
 console.log(hasPromotion);
